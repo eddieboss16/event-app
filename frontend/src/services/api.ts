6ce994b1447a9e61d144836c55b399a5d7332cc5
edu <edwinnjoroge001@gmail.com>
2025-09-03 00:10:30 +0300

    services/api complete

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,11 @@
+import axios, { AxiosInstance } from "axios";
+
+class ApiService {
+    private api: AxiosInstance;
+
+    constructor() {
+        this.api = axios.create({
+            
+        })
+    }
+}
